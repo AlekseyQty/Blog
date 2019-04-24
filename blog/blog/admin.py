a792902eca1c1c54f2831750e144f25930be4c92
AlekseyQty <saratkin.aleksey@gmail.com>
2019-04-25 00:40:22 +0300

    Added hiding/showing posts through admin app

--- blog/blog/admin.py
+++ blog/blog/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+
+def hide(modeladmin, request, queryset):
+    queryset.update(is_hidden='True')
+
+
+def show(modeladmin, request, queryset):
+    queryset.update(is_hidden='False')
+
+
+hide.short_description = "Hide post"
+show.short_description = "Show post"
+
+
+class PostAdmin(admin.ModelAdmin):
+    actions = [hide, show]
+
+
+admin.site.register(Post, PostAdmin)
--- blog/blog/models.py
+++ blog/blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     post_title = models.CharField(max_length=100)
     post_text = models.CharField(max_length=500)
     post_date = models.DateTimeField(default=datetime.now)
+    is_hidden = models.BooleanField(default=False)
 
     def __str__(self):
         return self.post_title
